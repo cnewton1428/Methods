940ba6e08857bceb05721a349e235928b87d1191
Carina <cncnewton37@gmail.com>
2018-10-10 17:01:15 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,67 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+//    public static void main(String[] args) {
+    // More Methods
+    //1.Add 2 numbers given by the user
+
+    Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Enter number 1: ");
+//        int num1 = input.nextInt();
+//
+//        System.out.println("Enter number 2:");
+//        int num2 = input.nextInt();
+//
+//        int sum = AddTwoNumbers(num1, num2);
+//        System.out.println(sum);
+//    }
+//        public static int AddTwoNumbers(int num1, int num2){
+//            int sum =0;
+//            sum = num1+num2;
+//
+//            return sum;
+//
+    //2. Get 5 letters from user, output them sorted(Hint: use ArrayList and Collections.sort()
+
+//        ArrayList<String> letters = new ArrayList<>();
+//
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println("Enter any letter");
+//            String letter = input.nextLine();
+//
+//            letters.add(letter);
+//        }
+//        //Statement to sort
+//        Collections.sort(letters);
+//        //Sorted List
+//        System.out.println("After Sorting");
+//        for (String s: letters) {
+//            System.out.println(s);
+//        }
+//    }
+
+    //3. Get the length of a string
+
+//    }
+public static void main(String[] args) {
+
+    public int length
+
+        String list1 = "Past";
+        String list2 = "Present";
+        String list3 = "Future";
+        System.out.println("String length is: " + list1.length());
+        System.out.print("String length is:" + list2.length());
+        System.out.println("String length is:" + list3.length());
+        return 0;
+    }
+}
+
+
+
